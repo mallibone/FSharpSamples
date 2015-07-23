c7f7ba8a9165867f11428d7538d3e5f7529fab54
mallibone <sirkenix@gmail.com>
2015-07-23 22:44:14 +0200

    Adds unit test for cars controller get

---
+++ FSharpWebSample.Test/CarsControllerTest.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharpWebSample.Test
+open Xunit
+open FSharpWebSample.Controllers
+
+module CarsControllerTest =
+    [<Fact>]
+    let Get_WhenInvoked_ReturnsAListContainingMultipleElements() = 
+        let carsController = new CarsController()
+        let cars = carsController.Get()
+        Assert.False(cars.IsEmpty)
