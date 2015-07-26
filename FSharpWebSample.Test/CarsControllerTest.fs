19e02a3e5c1b8f47ccc6408b047460ff243d65df
mallibone <sirkenix@gmail.com>
2015-07-26 08:28:02 +0200

    Test refactorings

--- FSharpWebSample.Test/CarsControllerTest.fs
+++ FSharpWebSample.Test/CarsControllerTest.fs
@@ -3,14 +3,15 @@ open Xunit
 open FSharpWebSample.Controllers
 
 module CarsControllerTest =
+
+    let carsController = new CarsController()
+
     [<Fact>]
     let Get_WhenInvoked_ReturnsAListContainingMultipleElements() = 
-        let carsController = new CarsController()
         let cars = carsController.Get()
         Assert.False(cars.IsEmpty)
 
     [<Fact>]
     let GetWithIndex_WhenInvokedWithAValidIndex_ReturnsASingleItem() = 
-        let carsController = new CarsController()
         let car = carsController.Get(2)
         Assert.True(car.Id = 2)
